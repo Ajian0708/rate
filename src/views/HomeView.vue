9d05d8111f309f16f480b0616add303313e2f5b2
samuelgoldenf <96034619+samuelgoldenf@users.noreply.github.com>
2022-03-03 15:16:01 +0800

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import Rates from '@/components/rate.vue'
+export default {  
+  components: {
+    Rates
+  }
+}
+</script>
+
+<template>
+  <div class="home">
+    
+    <Rates />
+
+  </div>
+</template>
